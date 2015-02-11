5c3d1196fa592bf5c51a0466566a03fb234f1f48
brett ohland <bohland@mobiquityinc.com>
2015-02-11 15:26:58 -0500

    Actually using the bugfix work in the bugfix script

--- createBugfixBranch.py
+++ createBugfixBranch.py
@@ -7,7 +7,7 @@ def todays_date_as_string():
 	return cur_date.strftime("%y%m%d")
 
 def branch_with_ticket(ticket_info):
-	return 'feature/'+ getpass.getuser() +'/' + todays_date_as_string() + '-' + ticket_info 
+	return 'bugfix/'+ getpass.getuser() +'/' + todays_date_as_string() + '-' + ticket_info 
 
 def create_new_branch():
 	sb.call(['git', 'checkout', '-b', branch_with_ticket(sys.argv[1])])
