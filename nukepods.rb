067271330904b7717795da5ff6a2cc41051e8dc1
brett ohland <bohland@mobiquityinc.com>
2015-05-08 09:56:38 -0400

    Letting the user know what's going on

--- nukepods.rb
+++ nukepods.rb
@@ -15,8 +15,12 @@ podfile_path = Pathname.new(podfile)
 pods_path = Pathname.new(pods)
  
 abort("This project isn't using Cocoapods, aborting") unless podfile_path.exist? and pods_path.exist?
- 
+
+puts("I say we take off and nuke the entire site from orbit. It's the only way to be sure.")
+
 podfile_path.delete() if podfile_path.exist?
 pods_path.rmtree if pods_path.exist?
+
+puts('Deleted Podfile.lock and /pods. Running `pod install`')
  
 system 'pod install' unless podfile_path.exist? and pods_path.exist?
