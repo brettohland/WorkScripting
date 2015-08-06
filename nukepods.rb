07c8efed36e6f15f00a3599ba344828a82d327df
brett ohland <bohland@mobiquityinc.com>
2015-08-06 13:19:10 -0400

    Adding more emoji

--- nukepods.rb
+++ nukepods.rb
@@ -16,7 +16,7 @@ pods_path = Pathname.new(pods)
  
 abort("This project isn't using Cocoapods, aborting") unless podfile_path.exist? and pods_path.exist?
 
-puts("I say we take off and nuke the entire site from orbit. It's the only way to be sure.")
+puts("\nI say we take off and nuke the entire site from orbit. It's the only way to be sure. ☢🚀:\n")
 
 podfile_path.delete() if podfile_path.exist?
 pods_path.rmtree if pods_path.exist?
